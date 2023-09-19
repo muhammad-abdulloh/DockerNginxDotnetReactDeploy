1f4b293de436a595d020a2bdb2f840584dc0a67a
muhammad-abdulloh <clevercoderr@gmail.com>
2023-09-19 15:11:46 +0500

    Add project files.

---
+++ DeployTest/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeployTest.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet] 
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new {Name = "Muhammad Abdulloh"},
+                new {Name = "Muhammad Yusuf"}
+            };
+
+            return Ok(users);
+        }
+    }
+}
---
+++ DeployTest/Startup.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeployTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options => options.AddPolicy( "CorsPolicy", builder =>
+            {
+                builder.WithOrigins("http://localhost:3000");
+
+                //builder.WithOrigins("http://localhost:5001");
+                builder.WithOrigins("http://localhost:5000");
+                //builder.WithOrigins("http://localhost:3001");
+            } ) );
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DeployTest", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DeployTest v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
